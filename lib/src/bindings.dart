87daeb2b796c95e4fcd5c613d240d16c90460857
Maxim Kuzin <m.kuzin@pmobi.tech>
2022-06-07 15:57:01 +0700

    Added support for windows

--- lib/src/bindings.dart
+++ lib/src/bindings.dart
@@ -27,6 +27,8 @@ abstract class FlutterLedgerLib {
       return DynamicLibrary.open('flutter_ledger_lib.so');
     } else if (Platform.isIOS || Platform.isMacOS) {
       return DynamicLibrary.process();
+    } else if (Platform.isWindows) {
+      return DynamicLibrary.open('flutter_ledger_lib.dll');
     } else {
       throw Exception('Invalid platform');
     }
